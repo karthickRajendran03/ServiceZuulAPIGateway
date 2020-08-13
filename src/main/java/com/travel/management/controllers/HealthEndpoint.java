8e523844f504337abb5feb233485824870998fa9
KarthickRajendran03 <karthick.sacs@gmail.com>
2020-08-13 15:16:34 +0530

    addd

--- src/main/java/com/travel/management/controllers/HealthEndpoint.java
+++ src/main/java/com/travel/management/controllers/HealthEndpoint.java
@@ -1,4 +1,4 @@
-package com.cognizant.travel.management.controllers;
+package com.travel.management.controllers;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
